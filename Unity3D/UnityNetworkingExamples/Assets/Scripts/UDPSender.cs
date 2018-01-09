8ca6ba0140b3ca5290fd0461d90e23148d00f9c8
Ben V <van.citters.b@gmail.com>
2018-01-09 13:39:34 -0800

    setup a working udp send example

--- Unity3D/UnityNetworkingExamples/Assets/Scripts/UDPSender.cs
+++ Unity3D/UnityNetworkingExamples/Assets/Scripts/UDPSender.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
 
-public class UDPSender : MonoBehaviour {
+public class UDPSender : MonoBehaviour 
+{
+    public string IPAddressString = "10.102.52.201";
+    public int EndpointPort = 5006; 
+    public string MessageString = "Hello from Unity3D!";
+	
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+    {
+        UDPSend();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	
+	void Update () 
+    {
 		
 	}
+
+    void UDPSend()
+    {
+        //init socket object
+        Socket sock = new Socket(AddressFamily.InterNetwork, 
+                                 SocketType.Dgram,
+                                 ProtocolType.Udp);
+
+        //parse the IP address
+        IPAddress serverAddr = IPAddress.Parse(IPAddressString);
+
+        IPEndPoint endPoint = new IPEndPoint(serverAddr, EndpointPort);
+
+        //convert to bytes
+        byte[] send_buffer = Encoding.ASCII.GetBytes(MessageString);
+
+        sock.SendTo(send_buffer, endPoint);
+
+        //close up socket object
+        sock.Close();
+    }
 }
