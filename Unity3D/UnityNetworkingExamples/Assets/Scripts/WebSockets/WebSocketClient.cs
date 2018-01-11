d8d145fb1ae9fc2913220a899078ae8181004a5b
Ben V <van.citters.b@gmail.com>
2018-01-10 18:40:24 -0800

    added a comment and added a appid so that the unity can be built to android

--- Unity3D/UnityNetworkingExamples/Assets/Scripts/WebSockets/WebSocketClient.cs
+++ Unity3D/UnityNetworkingExamples/Assets/Scripts/WebSockets/WebSocketClient.cs
@@ -16,6 +16,7 @@ public class WebSocketClient : MonoBehaviour
 	void Start () 
     {
         InitAndConnectWebSocket();
+        //test send message
         SendMessageToSocket("SENDERER");
 	}
 
